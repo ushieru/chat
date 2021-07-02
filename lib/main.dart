e8e333ff421672928692e75687b85825463361b9
Ushieru <uzielcocolan@gmail.com>
2021-07-02 18:54:42 -0500

    add home domain

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,6 @@ import 'package:chat/context/socketio_wraper/socket_io_wrapper.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  SocketIOWrapper('192.168.100.3', 5000, onConnect: () => print('[IO] Connected'));
+  SocketIOWrapper('127.0.0.1', 5000, onConnect: () => print('[IO] Connected'));
   runApp(ChatApp());
 }
