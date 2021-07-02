907da30960b4e6b34c2956c7b0b5948dbe3cb0a5
Ushieru <uzielcocolan@gmail.com>
2021-07-02 18:02:20 -0500

    init socket io

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:chat/app/chat_app.dart';
+import 'package:chat/context/socketio_wraper/socket_io_wrapper.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  SocketIOWrapper('192.168.100.3', 5000, onConnect: () => print('[IO] Connected'));
   runApp(ChatApp());
 }
