77f9d7eab9bc03de8e18c1f823a62f64e6366dc9
Ushieru <uzielcocolan@gmail.com>
2021-07-02 13:48:15 -0500

    chat screen

--- lib/app/screens/chat/chat_screen.dart
+++ lib/app/screens/chat/chat_screen.dart
@@ -1,5 +1,8 @@
 library chat_screen;
 
+import 'package:chat/app/widgets/in_chat_header.dart';
+import 'package:chat/app/widgets/message.dart';
+import 'package:chat/context/message/domain/message.dart' as Domain;
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -17,7 +20,52 @@ class ChatScreen extends GetResponsiveView<ChatScreenController> {
       body: SafeArea(
         child: Column(
           children: [
-            
+            InChatHeader(),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(15),
+                child: Obx(
+                  () => ListView(
+                    children: List.generate(
+                      controller.messages.length,
+                      (index) => Message(
+                        message: controller.messages[index],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Divider(),
+            Container(
+              margin: const EdgeInsets.all(10),
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      minLines: 1,
+                      maxLines: 3,
+                      controller: controller.messageController,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: controller.sendMessage,
+                    icon: Icon(
+                      Icons.send,
+                      color: Colors.blue,
+                    ),
+                  ),
+                ],
+              ),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey.shade300),
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+              ),
+            ),
           ],
         ),
       ),
--- lib/app/screens/chat/controllers/chat_screen_controller.dart
+++ lib/app/screens/chat/controllers/chat_screen_controller.dart
@@ -1,3 +1,16 @@
 part of chat_screen;
 
-class ChatScreenController extends GetxController {}
+class ChatScreenController extends GetxController {
+  TextEditingController messageController = TextEditingController();
+  RxList<Domain.Message> messages = <Domain.Message>[].obs;
+
+  ChatScreenController() {
+    this.messages.add(Domain.Message(false, 'Hey! What\'s up?'));
+    this.messages.add(Domain.Message(true, 'Hello bro!'));
+  }
+
+  void sendMessage() {
+    this.messages.add(Domain.Message(true, this.messageController.text));
+    this.messageController.text = '';
+  }
+}
